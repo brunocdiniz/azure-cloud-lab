df06d73f34b86228281faf0c0e2636bb3c9a46ba
Bruno Caetano Diniz <49279559+brunocdiniz@users.noreply.github.com>
2026-04-21 04:17:30 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const port = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.end(`
+    <h1>Azure Cloud Lab 🚀</h1>
+    <p>Projeto do Bruno no Azure App Service</p>
+    <p>Arquitetura Hub-Spoke + GitHub Integration</p>
+  `);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
