b8e56fb4c9c9555f8cbc03d0ef91ac7985faabfd
Bruno Caetano Diniz <49279559+brunocdiniz@users.noreply.github.com>
2026-04-21 04:18:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.end(`
     <h1>Azure Cloud Lab 🚀</h1>
-    <p>Projeto do Bruno no Azure App Service</p>
+    <p>Projeto pessoal no Azure App Service</p>
     <p>Arquitetura Hub-Spoke + GitHub Integration</p>
   `);
 });
